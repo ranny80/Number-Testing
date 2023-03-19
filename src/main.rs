937fb513583ce294c4440e9693c2ba27c55dd045
ranny80 <rannyvuong80@gmail.com>
2023-03-19 16:08:15 +0000

    Added comments

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 fn incorrectanswer() {
-  const MSG: &str = "That is incorrect.";
+  const MSG: &str = "That is incorrect."; // It tells that the number is incorrect
 
   println!("{}", MSG);
 }
 
 fn main() {
-  let number = 3;
+  let number = 3; // Type a number anything you want
   
   if number == 3 {
-    println!("Correct!");
+    println!("Correct!"); // It will asked you that the answer is correct
   }
   else {
-    incorrectanswer();
+    incorrectanswer(); // It will asked you that the answer is incorrect
   } 
 }
